b1c3bc2de26c092db70ebee4316f99b293027648
Iza <i.baszczewska@gmail.com>
2020-11-13 00:29:29 +0100

    zadanie2_IzaW

--- src/test/java/zadanie2/KatalonTest.java
+++ src/test/java/zadanie2/KatalonTest.java
@@ -29,43 +29,62 @@ public class KatalonTest {
 
     @Test
     public void fillFormTest() {
-        // przygotowanie danych
-        String firstNameText = "Jan";
-        String lastNameText = "Kowalski";
-
-        // znajdz i wypelnij First name
+        // znajdź i wypełnij First name
         WebElement firstNameInput = driver.findElement(By.id("first-name"));
-        if (firstNameInput.isDisplayed()) {
-            firstNameInput.sendKeys(firstNameText);
-            WebElement firstNameLabel = driver.findElement(By.cssSelector("label[for='first-name']"));
-            System.out.println(firstNameLabel.getText() + " : " + firstNameText);
-        } else {
-            Assert.fail("firstNameInput is not displayed.");
-        }
+        firstNameInput.sendKeys("Karol");
 
-        // znajdz i wypelnij Last name
+        //znajdź i wypełnij Last name
         WebElement lastNameInput = driver.findElement(By.id("last-name"));
-        if (lastNameInput.isDisplayed()) {
-            lastNameInput.sendKeys(lastNameText);
-            WebElement lastNameLabel = driver.findElement(By.cssSelector("label[for='last-name']"));
-            System.out.println(lastNameLabel.getText() + " : " + lastNameText);
-        } else {
-            Assert.fail("lastNameInput is not displayed.");
-        }
+        lastNameInput.sendKeys("Kowalski");
 
-        // wybierz male
+        // Wybierz male
         WebElement maleRadioButton = driver.findElement(By.xpath("//*[@id=\"infoForm\"]/div[3]/div/div/label[1]/input"));
         maleRadioButton.click();
 
-        // select
+        //Znajdź i wypełnij date of birth
+        WebElement dateOfBirthInput = driver.findElement(By.id("dob"));
+        dateOfBirthInput.sendKeys("05/22/2010");
+
+        //Znajdź i wypełnij address
+        WebElement addressInput = driver.findElement(By.id("address"));
+        addressInput.sendKeys("Prosta 51");
+
+        //Znajdź i wypełnij adres email
+        WebElement addressEmail = driver.findElement(By.id("email"));
+        addressEmail.sendKeys("karol.kowalski@mailinator.com");
+
+        //Znajdź i wypełnij password
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("Pass123");
+
+        //Znajdź i wypełnij Company
+        WebElement company = driver.findElement(By.id("company"));
+        company.sendKeys("Coders Lab");
+
+        //Select role
         Select roleDropdown = new Select(driver.findElement(By.id("role")));
         roleDropdown.selectByVisibleText("QA");
+
+        //Job expectation
+        Select jobExpectationDropdown = new Select(driver.findElement(By.id("expectation")));
+        jobExpectationDropdown.selectByVisibleText("Challenging");
+
+        //Ways of development
+        WebElement developmentButton = driver.findElement(By.xpath("//*[@id=\"infoForm\"]/div[11]/div/div[1]/label"));
+        developmentButton.click();
+        WebElement developmentButton2 = driver.findElement(By.xpath("//*[@id=\"infoForm\"]/div[11]/div/div[2]/label"));
+        developmentButton2.click();
+
+        //Znajdź i wypełnij Comment
+        WebElement comment = driver.findElement(By.id("comment"));
+        comment.sendKeys("To jest mój pierwszy automat testowy");
 
         driver.findElement(By.id("submit")).click();
     }
 
     @After
-    public void tearDown() {
-        driver.quit();
+    public void tearDown() throws Exception {
+        // Zamknij przeglądarkę
+        //driver.quit();
     }
 }
